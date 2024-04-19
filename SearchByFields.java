4c34cd4cd82f479d3d7194a965e794f803b7bde1
Greenberg001 <bergcampjoe@gmail.com>
2024-04-18 22:51:46 -0400

    finish cleanup code

--- SearchByFields.java
+++ SearchByFields.java
@@ -5,105 +5,6 @@ import java.sql.SQLException;
 
 public class SearchByFields {
 
-//    public ResultSet searchFields(String author, String title, String year, String type, String sort, boolean out_ID,
-//                             boolean out_author, boolean out_title, boolean out_year, boolean out_type, boolean summary, Connection conn) throws SQLException {
-//
-//        String getByFields = "Select ?, ?, ?, ?, ?, ? from Publications p join Authors a on p.publicationID = a.publicationID " +
-//                " where author LIKE ? or title LIKE ? or Year LIKE ? or Type LIKE ? order by ?";
-//
-//        PreparedStatement queryFields = conn.prepareStatement(getByFields);
-//
-//        String a = author == null ? "" : author;
-//        String t = title == null ? "" : title;
-//        String y = year == null ? "" : year;
-//        String ty = type == null ? "" : type;
-////        String a = "%" + author + "%";
-////        String t = "title Like '%" + title + "%' ";
-////        String y = "year Like '%" + year + "%' ";
-////        String ty = "type Like '%" + type + "%' ";
-//        String order = sort == null ? "" : sort;
-//
-//        StringBuilder sb = new StringBuilder();
-//
-//        String outId = out_ID == false ? "" : "publicationID, ";
-//        String outA = out_author == false ? "" : "author, ";
-//        String outT = out_title == false ? "" : "title, ";
-//        String outY = out_year == false ? "" : "year, ";
-//        String outType = out_type == false ? "" : "type, ";
-//        String outS = summary == false ? "" : "summary";
-//
-//        String[] selectedFields = {outId, outA, outT, outY, outType, outS};
-//        for (int i = 0; i < selectedFields.length; i++) {
-//            if (selectedFields[i] == "") {
-//                continue;
-//            }
-//            sb.append(selectedFields[i]);
-//            if (i < selectedFields.length - 1) {
-//                sb.append(", ");
-//            }
-//        }
-//
-//
-//        queryFields.setString(1, outId);
-//        queryFields.setString(2, outA);
-//        queryFields.setString(3, outT);
-//        queryFields.setString(4, outY);
-//        queryFields.setString(5, outType);
-//        queryFields.setString(6, outS);
-//
-//        queryFields.setString(7, "%" + a + "%");
-//        System.out.println("%" + author + "%");
-//        queryFields.setString(8, "%" + t + "%");
-//        queryFields.setString(9, "%" + y + "%");
-//        queryFields.setString(10, "%" + ty + "%");
-//        queryFields.setString(11, order);
-//
-//        ResultSet res = queryFields.executeQuery();
-//        while (res.next()) {
-//            System.out.println(res.getString(2));
-//        }
-//
-//        return res;
-//
-//    }
-
-
-//    public ResultSet searchFields(String author, String title, String year, String type, String sort, boolean out_ID,
-//                             boolean out_author, boolean out_title, boolean out_year, boolean out_type, boolean summary, Connection conn) throws SQLException {
-//        // 动态构建需要选择的列
-//        StringBuilder fields = new StringBuilder();
-//        if (out_ID) fields.append("publicationID, ");
-//        if (out_author) fields.append("a.author, ");
-//        if (out_title) fields.append("title, ");
-//        if (out_year) fields.append("year, ");
-//        if (out_type) fields.append("type, ");
-//        if (summary) fields.append("summary, ");
-//        if (fields.length() > 0) fields.delete(fields.length() - 2, fields.length());  // 删除最后的逗号和空格
-//
-//        // 构建查询语句
-//        String getByFields = "SELECT " + fields.toString() + " FROM Publications p JOIN Authors a ON p.publicationID = a.publicationID " +
-//                "WHERE a.author LIKE ? OR title LIKE ? OR year LIKE ? OR type LIKE ? " +
-//                (sort.isEmpty() ? "" : "ORDER BY " + sort);  // 注意: 这可能引入SQL注入风险
-//
-//        ResultSet res;
-//        try (PreparedStatement queryFields = conn.prepareStatement(getByFields)) {
-//            queryFields.setString(1, "%" + (author == null ? "" : author) + "%");
-//            queryFields.setString(2, "%" + (title == null ? "" : title) + "%");
-//            queryFields.setString(3, "%" + (year == null ? "" : year) + "%");
-//            queryFields.setString(4, "%" + (type == null ? "" : type) + "%");
-//
-//            res = queryFields.executeQuery();
-////            while (res.next()) {
-////                for (int i = 1; i <= res.getMetaData().getColumnCount(); i++) {
-////                    System.out.print(res.getString(i) + " ");
-////                }
-////                System.out.println();  // 打印每行结果
-////            }
-//
-//        }
-//        return res;
-//    }
-
     public ResultSet searchFields(String author, String title, String year, String type, String sort,
                              boolean out_ID, boolean out_author, boolean out_title,
                              boolean out_year, boolean out_type, boolean summary, Connection conn) throws SQLException {
@@ -164,23 +65,6 @@ public class SearchByFields {
 //            }
 
         return res;
-//        try (PreparedStatement queryFields = conn.prepareStatement(sql)) {
-//            int index = 1;
-//            if (author != null) queryFields.setString(index++, "%" + author + "%");
-//            if (title != null) queryFields.setString(index++, "%" + title + "%");
-//            if (year != null) queryFields.setString(index++, "%" + year + "%");
-//            if (type != null) queryFields.setString(index++, "%" + type + "%");
-//
-//            ResultSet res = queryFields.executeQuery();
-////            while (res.next()) {
-////                for (int i = 1; i <= res.getMetaData().getColumnCount(); i++) {
-////                    System.out.print(res.getString(i) + " ");
-////                }
-////                System.out.println();  // 打印每行结果
-////            }
-//            return res;
-//        }
-
     }
 
 
